bf1d28f5dc11b5c3232f6d2bcea9b3c2726fd168
JFmaia <jfmaia741@gmail.com>
2023-09-28 19:36:35 -0300

    Criando um arquitetura de pastas

--- 01-GRAPHQL/src/index.js
+++ 01-GRAPHQL/src/index.js
@@ -1,44 +1,8 @@
-import { ApolloServer, gql } from 'apollo-server';
-//Type content complexos
+import { ApolloServer } from 'apollo-server';
+import { resolvers, typeDefs } from './graphql/schema';
 const server = new ApolloServer({
-  typeDefs: gql`
-    type Query {
-      user: User!
-      users: [User!]!
-    }
-
-    type User {
-      id: ID!
-      userName: String
-    }
-  `,
-  // o ! na frente do type não deixa ser null e ele tem que existir
-  resolvers: {
-    Query: {
-      user: () => {
-        return {
-          id: 'sfdhusfus',
-          userName: 'José Flavio',
-        };
-      },
-      users: () => {
-        return [
-          {
-            id: 'sfdhusfus',
-            userName: 'José Flavio',
-          },
-          {
-            id: 'wser42423',
-            userName: 'Matheus Dantas',
-          },
-          {
-            id: 'wer324wdsrfwe',
-            userName: 'José Maia',
-          },
-        ];
-      },
-    },
-  },
+  typeDefs,
+  resolvers,
 });
 
 server.listen(4003).then(({ url }) => {
