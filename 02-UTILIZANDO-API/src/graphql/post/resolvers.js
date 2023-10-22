cadfeb254254b02ec2bdf5db84a755ed153cafa0
JFmaia <jfmaia.dev@gmail.com>
2023-10-22 20:28:58 -0300

    Modificando resolvers do post, para ficarem dinamicos.

--- 02-UTILIZANDO-API/src/graphql/post/resolvers.js
+++ 02-UTILIZANDO-API/src/graphql/post/resolvers.js
@@ -1,26 +1,16 @@
+const post = async (_, { id }, { axios, urlBase }) => {
+  const post = await axios.get(`${urlBase}posts/` + id);
+  return post.data;
+};
+
+const posts = async (_, __, { axios, urlBase }) => {
+  const posts = await axios.get(`${urlBase}posts`);
+  return posts.data;
+};
+
 export const postResolvers = {
   Query: {
-    post: () => {
-      return {
-        id: 'ahddjah2344j',
-        title: 'Tudo vai acabar amanhã!!',
-      };
-    },
-    posts: () => {
-      return [
-        {
-          id: 'ahddjah2344j',
-          title: 'Tudo vai acabar amanhã!!',
-        },
-        {
-          id: 'ahddjah2444j',
-          title: 'Coneça murilo o menino mais forte do mundo!!',
-        },
-        {
-          id: 'ahddjah2374j',
-          title: 'Tudo vai acabar amanhã as 9:00!!',
-        },
-      ];
-    },
+    post,
+    posts,
   },
 };
